9636b57612fde0f78d94077c8f30fb57a7213773
Brian Masney <masneyb@onstation.org>
2016-12-14 03:46:46 -0500

    corrected the spatial reference ID of the hillshade files

--- bin/us_wv/create-us-wv-hillshade.sh
+++ bin/us_wv/create-us-wv-hillshade.sh
@@ -46,17 +46,11 @@ LAYER
 		MAXSCALE 60000.00
 	END
 	PROJECTION
-		"+proj=merc"
-		"+a=6378137"
-		"+b=6378137"
-		"+lat_ts=0.0"
-		"+lon_0=0.0"
-		"+x_0=0.0"
-		"+y_0=0"
-		"+k=1.0"
+		"+proj=utm"
+		"+zone=17"
+		"+ellps=GRS80"
+		"+datum=NAD83"
 		"+units=m"
-		"+nadgrids=@null"
-		"+wktext"
 		"+no_defs"
 	END
 END
